bd75efa0f36eaf4a91d201f15d51926e8b5714e8
aman-singh1601 <aman16singh2001@gmail.com>
2024-04-27 15:09:50 +0530

    added landing card

--- components/LandingCard/index.tsx
+++ components/LandingCard/index.tsx
@@ -3,10 +3,21 @@ import React from 'react'
 
 export default function LandingCard() {
   return (
-    <View>
+    <View style = {[styles.landingCard]}>
       <Text>LandingCard</Text>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  landingCard: {
+    width: '90%',
+    height: 600,
+    backgroundColor: "#6c37a3",
+    borderRadius: 20,
+    padding: 10,
+    margin: "auto",
+    opacity: 0.8
+
+  }
+})
